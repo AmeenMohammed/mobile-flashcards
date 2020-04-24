a5006201a81257bf484ce147c6968d03d648fe06
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-04-24 17:28:38 +0200

    Add status bar

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import {  View } from 'react-native'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import middleware from './middleware'
 import { createStore } from 'redux'
 import DecksList from './components/DecksList'
+import { black } from './utils/colors'
+import Constants from 'expo-constants'
+import { StatusBar } from 'react-native'
 
 const store = createStore(reducer, middleware)
+
+function UdaciStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 export default class App extends React.Component  {
   render(){
     return (
       <Provider store={store}>
-      <View style={styles.container}>
+      <View style={{flex:1}}>
+      <UdaciStatusBar backgroundColor={black} barStyle="light-content"/>
         <DecksList />
       </View>
       </Provider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
